2087ebf1ef1087ebecb69aa6ee4b1503107147a2
nutman <juncker88@mail.ru>
2016-01-13 15:56:00 +0200

    New: 5 min start is ready

--- FriendsService.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by vovchik on 8/28/15.
- */
-var FriendsService = (function () {
-    function FriendsService() {
-        this.names = ["Alice", "Aarav", "Martín", "Shannon", "Ariana", "Kai"];
-    }
-    return FriendsService;
-})();
